458c2e1bddb367083ea676330558f589a6fc4fd5
zcae1020 <zcae1020@naver.com>
2022-09-07 10:53:12 +0900

    add index.html

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var indexRouter = require('./routes/index')
+var adminRouter = require('./routes/admin')
 
 var app = express()
 
 app.use('/', indexRouter)
+app.use('/admin', adminRouter)
 
 app.listen(5000)
