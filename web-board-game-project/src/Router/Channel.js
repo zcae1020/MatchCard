d8165b9a409872fddf473f7bebc7ca335aa4145a
김준하 <junha@gimjunhaui-MacBookAir.local>
2022-10-10 16:49:20 +0900

    20221010

--- web-board-game-project/src/Router/Channel.js
+++ web-board-game-project/src/Router/Channel.js
@@ -1,5 +1,27 @@
+import { style } from '@mui/system';
 import React from 'react';
+import styles from '../css/ChannelAndRoom.module.css';
 
-function Channel() {}
+function Channel() {
+  return (
+    <div>
+      <h1>Channels</h1>
+      <div className={styles.channel_row}>
+        <span className={styles.channel_span}>
+          <span className={styles.channel_num}>Channel1</span>
+        </span>
+        <span className={styles.channel_span}>Channel2</span>
+      </div>
+      <div className={styles.channel_row}>
+        <span className={styles.channel_span}>Channel3</span>
+        <span className={styles.channel_span}>Channel4</span>
+      </div>
+      <div className={styles.channel_row}>
+        <span className={styles.channel_span}>Channel5</span>
+        <span className={styles.channel_span}>Channel6</span>
+      </div>
+    </div>
+  );
+}
 
 export default Channel;
