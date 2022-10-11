31931ca9806ca5e37da654ac168b7edf47af2db3
김준하 <junha@gimjunhaui-MacBookAir.local>
2022-10-11 23:21:34 +0900

    server

---
+++ client/src/Router/Login.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import styles from '../css/Login.module.css';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../firebase';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+
+const auth = getAuth(app);
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userType, setUserType] = React.useState('player');
+
+  const handleChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const signin = async () => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user.accessToken);
+        console.log(result.user.displayName);
+        console.log(result.user.uid);
+        navigate('/Channel');
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/wrong-password':
+            setErrorMessage('*비밀번호가 일치하지 않습니다.');
+            break;
+          case 'auth/invalid-email':
+            setErrorMessage('*잘못된 이메일 주소입니다');
+            break;
+          case 'auth/user-not-found':
+            setErrorMessage('*가입되지 않은 이메일입니다.');
+            break;
+        }
+
+        console.log(error);
+      });
+  };
+
+  const onChange = (e) => {
+    if (e.target.id === 'textfield-id') setEmail(e.target.value);
+    else setPassword(e.target.value);
+    console.log(email);
+    console.log(password);
+  };
+
+  const onClick = (e) => {
+    e.preventDefault();
+    signin();
+  };
+
+  return (
+    <div>
+      <h1>Match Card Game</h1>
+      <div className={styles.div_login}>
+        <Box
+          className={styles.div_box_login}
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <FormControl>
+            <RadioGroup
+              row
+              name="controlled-radio-buttons-group"
+              value={userType}
+              onChange={handleChange}
+              className={styles.radio_userType}
+            >
+              <FormControlLabel
+                value="player"
+                control={<Radio />}
+                label="Player"
+                labelPlacement="top"
+              />
+              <FormControlLabel
+                value="admin"
+                control={<Radio />}
+                label="Admin"
+                labelPlacement="top"
+              />
+            </RadioGroup>
+          </FormControl>
+          <TextField
+            id="textfield-id"
+            label="E-MAIL"
+            variant="standard"
+            onChange={onChange}
+          />
+          <TextField
+            id="textfield-password"
+            label="PASSWORD"
+            variant="standard"
+            type="password"
+            onChange={onChange}
+          />
+          <button className={styles.box_login_button} onClick={onClick}>
+            login
+          </button>
+        </Box>
+        <p className={styles.errorMessage}>{errorMessage}</p>
+        <p>
+          <Link to={'/SignUp'}>signup</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
