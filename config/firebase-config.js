59375e847a5bf71314c237cbbbeb5c756e5394b3
zcae1020 <zcae1020@naver.com>
2022-11-09 15:31:04 +0900

    fix(serviceAccount.js): add env

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "./serviceAccount.json" assert { type: "json" };
+import serviceAccount from "./serviceAccount.js"
 
 initializeApp({
   credential: cert(serviceAccount),
---
+++ config/serviceAccount.js
@@ -0,0 +1,12 @@
+export const serviceAccount = {
+  "type": "service_account",
+  "project_id": "match-card-dcd2e",
+  "private_key_id": process.env.PK_ID,
+  "private_key": "-----BEGIN PRIVATE KEY-----\n"+process.env.PK+"\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-k6k6x@match-card-dcd2e.iam.gserviceaccount.com",
+  "client_id": process.env.CI,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-k6k6x%40match-card-dcd2e.iam.gserviceaccount.com"
+}
