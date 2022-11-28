eed8da3a8d45c43571869e9bf0f77f51fb58483d
zcae1020 <zcae1020@naver.com>
2022-11-28 23:16:35 +0900

    fix: connection안에 모든 것 넣도록 변경

--- server/inChannel.js
+++ server/inChannel.js
@@ -1,15 +1,15 @@
 import { enter } from "./controller/CHANNEL/enter.js";
 
-const onConnection = (socket) => {
+export const connection = (channelNamespace) => {
+  channelNamespace.on("connection", onConnection);
+
+  const onConnection = (socket) => {
     console.log("channel connect");
     socket.emit("channel connected");
     socket.on("success enter room", (a) => {
       console.log(a);
     });
 
-    enter(channelNamespace);
-}
-
-export const connection = (channelNamespace) => {
-  channelNamespace.on("connection", onConnection);
+    enter(channelNamespace, socket);
+  }
 };
